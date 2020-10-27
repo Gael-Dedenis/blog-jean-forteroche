122e3147171a14ab0dbda7e5976f26ae554c47eb
Dedenis <gaeldedenis01@laposte.net>
2020-10-27 08:53:35 +0100

    Edition Index.php (Appel autoload + création du routeur)

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
 
+    use App\Router;
+    use Tracy\Debugger;
+
+    /* Appel de l'autoload */
     require_once "../vendor/autoload.php";
 
+    /* Création du routeur */
+    $router = new Router();
+    $router->run();
 
-?>
+    Debugger::enable();
