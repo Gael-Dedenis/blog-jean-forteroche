844ece397b0822daf000eea2f2d92c2522b9183c
Dedenis <gaeldedenis01@laposte.net>
2020-11-03 08:51:19 +0100

    Création de la classe Router

--- src/Router.php
+++ src/Router.php
@@ -2,11 +2,11 @@
 
     namespace App\Controllers;
 
-    require_once "../config/config_dev.php";
-
     use Twig\Environment;
     use Twig\Extension\DebugExtension;
     use Twig\Loader\FilesystemLoader;
+
+    require_once "../config/config_dev.php";
 
     /**
      * Classe Router
@@ -14,11 +14,28 @@
      */
     class Router
     {
+        /**
+         * Controller requis.
+         * @var string
+         */
+        private $controller = DEFAULT_CONTROLLER ;
 
+        /**
+         * Method requise.
+         * @var string
+         */
+        private $method = DEFAULT_METHOD ;
+
+        /**
+         * Router constructor
+         * Mets en place l'environement, puis analyse l'URL, ensuite mets en place les controllers et les méthodes.
+         */
         public function __construct()
         {
             $this->setEnvironment();
-            $this->setHome();
+            $this->parseUrl();
+            $this->setController();
+            $this->setMethod();
         }
 
         /**
@@ -33,14 +50,63 @@
             ));
 
             $this->twig->addExtension(new DebugExtension());
-            $this-$twig->addFilter(FILTER_NL2BR);
+            $this->$twig->addFilter(FILTER_NL2BR);
         }
 
         /**
-         * Redirection sur la page d'accueil
+         * Analyse l'URL pour prendre le bon controleur et ses méthodes.
+         * @return mixed|void
          */
-        public function setHome()
+        public function parseUrl()
         {
-            
+            $access = filter_input(INPUT_GET, "access");
+
+            if (!isset($access)) {
+                $access = "home";
+            }
+
+            $access           = explode("!", $access);
+            $this->controller = $access[0];
+            $this->method     = count($access) == 1 ? "launch" : $access[1];
+        }
+
+        /**
+         * Créer les requêtes controller.
+         * @return mixed|void
+         */
+        public function setController()
+        {
+            $this->controller = ucfirst(\strtolower($this->controller)) . "controller";
+            $this->controller = self::DEFAULT_PATH . $this->controller;
+
+            if (!class_exists($this->controller)) {
+                $this->controller = self::DEFAULT_PATH . self::DEFAULT_CONTROLLER;
+            }
+        }
+
+        /**
+         * Créer les requêtes pour les méthodes.
+         * @return mixed|void
+         */
+        public function setMethod()
+        {
+            $this->method = strtolower($this->method) . "Method";
+
+            if (!method_exists($this->controller, $this->method))
+            {
+                $this->method = self::LAUNCH_METHOD;
+            }
+        }
+
+        /**
+         * Créer l'objet Controller et appel 
+         * @return mixed|void
+         */
+        public function run()
+        {
+            $this->controller = new $this->controller();
+            $response         = call_user_func([$this->controller, $this->method]);
+
+            echo filter_var($response);
         }
     }
