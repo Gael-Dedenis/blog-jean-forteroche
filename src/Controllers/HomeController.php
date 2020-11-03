b8bc9d291074f6b8bb60a5cbc3f53e740f3aa6c1
Dedenis <gaeldedenis01@laposte.net>
2020-11-03 08:54:53 +0100

    Création de la classe HomeController + launchMethod

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace App\Controllers;
+
+    use App\Models\Factory\ModelFactory;
+    use Twig\Error\LoaderError;
+    use Twig\Error\RuntimeError;
+    use Twig\Error\SyntaxError;
+
+    /**
+     * Classe HomeController
+     * @package App\Controllers
+     */
+    class HomeController extends MainController
+    {
+        /**
+         * @return string
+         * @throws LoaderError
+         * @throws RuntimeError
+         * @throws SyntaxError
+         */
+        public function launchMethod()
+        {
+            $lastPost = ModelFactory::getModel('Posts')->listData();
+
+            return $this->render('home.twig', ['posts' => $lastPost]);
+        }
+    }
