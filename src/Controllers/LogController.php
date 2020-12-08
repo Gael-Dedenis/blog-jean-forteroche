3398066b49e53c26e2be3cd08e77d597ac330945
Dedenis <gaeldedenis01@laposte.net>
2020-12-08 09:13:56 +0100

    Création LogController

--- src/Controllers/LogController.php
+++ src/Controllers/LogController.php
@@ -3,7 +3,7 @@
     namespace App\Controller;
 
     /**
-     * Class LogController
+     * Classe LogController
      * @package App\Controller
      */
     abstract class LogController
@@ -30,13 +30,15 @@
 
         /**
          * LogController constructor
+         * récupération des données dans les inputs (get ou post)
+         * 
          */
         public function __construct()
         {
             $this->get     = filter_input_array(INPUT_GET);
             $this->post    = filter_input_array(INPUT_POST);
 
-            $this->session = filter_data_array($_SESSION);
+            $this->session = filter_var_array($_SESSION);
             if (isset($this->session['user_data']))
             {
                 $this->user = $this->session['user_data'];
@@ -44,24 +46,26 @@
         }
 
         /**
+         * Création de la session avec les données de l'utilisateur
          * @param int $id
          * @param string $pseudo
          * @param string $email
          * @param string $password
          * @param string $status
          */
-        public function sessionCreate(int $id, string $pseudo, string $email, string $password,string $status)
+        public function sessionCreate(int $id, string $pseudo, string $email, string $password,string $admin)
         {
             $_SESSION['user_data'] = [
                 'id'       => $id,
                 'pseudo'   => $pseudo,
                 'email'    => $email,
                 'pass'     => $password,
-                'status'   => $status
+                'admin'    => $admin
             ];
         }
 
         /**
+         * détruit la session
          * @return void
          */
         public function sessionDestroy()
@@ -70,9 +74,10 @@
         }
 
         /**
+         * Vérifie que l'utilisateur est connecté
          * @return bool
          */
-        public function getLog()
+        public function isConnected()
         {
             if (array_key_exists('user_data', $this->session)) {
                 if (!empty($this->user)) {
@@ -88,7 +93,7 @@
          */
         public function getUser($data)
         {
-            if ($this->getLog() === false) {
+            if ($this->isConnected() === false) {
                 $this->user[$data] = null;
             }
             return $this->user[$data];
