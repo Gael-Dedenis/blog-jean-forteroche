e5a794fe3a1586a24d9714b8bf4223cd7257c534
Dedenis <gaeldedenis01@laposte.net>
2021-01-12 08:18:23 +0100

    Modifications syntaxiques et modification du Router

--- src/Router.php
+++ src/Router.php
@@ -4,12 +4,25 @@
 
     require_once "../config/config_dev.php";
 
+    use App\Controllers\ExtensionFeaturesTwig;
+    use Twig\Environment;
+    use Twig\Loader\FilesystemLoader;
+    use Twig\Extension\DebugExtension;
+    use Twig\Error\LoaderError;
+    use Twig\Error\RuntimeError;
+    use Twig\Error\SyntaxError;
+
     /**
      * Classe Router
      * @package App
      */
     class Router
     {
+        /**
+         * @var Environment
+         */
+        private $twig = null;
+
         /**
          * Controller requis.
          * @var string
@@ -26,19 +39,34 @@
          * Router constructor
          * Mets en place l'environement, puis analyse l'URL, ensuite mets en place les controllers et les méthodes.
          */
-        public function __construct()
-        {
+        public function __construct() {
+            $this->setEnvironment();
             $this->parseUrl();
             $this->setController();
             $this->setMethod();
         }
 
         /**
+         * Mise en place environement Twig.
+         * Ajout de la global Session aux Vues Twig.
+         * Ajouts de fonctionnalités pour les Vues Twig.
+         * @return mixed|void
+         */
+        private function setEnvironment() {
+            $this->twig = new Environment(new FilesystemLoader("../src/Views"), array(
+                "cache" => false,
+                "debug" => true
+            ));
+            $this->twig->addGlobal("session", $_SESSION);
+            $this->twig->addExtension(new \Twig\Extension\DebugExtension());
+            $this->twig->addExtension(new ExtensionFeaturesTwig());
+        }
+
+        /**
          * Analyse l'URL pour prendre le bon controleur et ses méthodes.
          * @return mixed|void
          */
-        public function parseUrl()
-        {
+        public function parseUrl() {
             $access = filter_input(INPUT_GET, "access");
 
             if (!isset($access)) {
@@ -47,15 +75,14 @@
 
             $access           = explode("!", $access);
             $this->controller = $access[0];
-            $this->method     = count($access) == 1 ? "launch" : $access[1];
+            $this->method     = count($access) == 1 ? "default" : $access[1];
         }
 
         /**
          * Créer les requêtes controller.
          * @return mixed|void
          */
-        public function setController()
-        {
+        public function setController() {
             $this->controller = ucfirst(\strtolower($this->controller)) . "controller";
             $this->controller = DEFAULT_PATH . $this->controller;
 
@@ -68,8 +95,7 @@
          * Créer les requêtes pour les méthodes.
          * @return mixed|void
          */
-        public function setMethod()
-        {
+        public function setMethod() {
             $this->method = strtolower($this->method) . "Method";
 
             if (!method_exists($this->controller, $this->method))
@@ -82,9 +108,8 @@
          * Créer l'objet Controller et appel 
          * @return mixed|void
          */
-        public function run()
-        {
-            $this->controller = new $this->controller();
+        public function run() {
+            $this->controller = new $this->controller($this->twig);
             $response         = call_user_func([$this->controller, $this->method]);
 
             echo filter_var($response);
